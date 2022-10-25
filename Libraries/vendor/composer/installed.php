4ffd8ac75fb0e9679d2619cdef8e609761d24ae0
leenh bustmante <hackingleenh@gmail.com>
2022-10-25 15:31:09 -0500

    bootstrap

--- Libraries/vendor/composer/installed.php
+++ Libraries/vendor/composer/installed.php
@@ -1,50 +1,67 @@
-<?php return array(
-    'root' => array(
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'type' => 'library',
-        'install_path' => __DIR__ . '/../../',
-        'aliases' => array(),
-        'reference' => '099e1e75ace0915fce8feb512bedae1f2be919b3',
-        'name' => '__root__',
-        'dev' => true,
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
     ),
-    'versions' => array(
-        '__root__' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../../',
-            'aliases' => array(),
-            'reference' => '099e1e75ace0915fce8feb512bedae1f2be919b3',
-            'dev_requirement' => false,
-        ),
-        'composer/ca-bundle' => array(
-            'pretty_version' => '1.3.3',
-            'version' => '1.3.3.0',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/./ca-bundle',
-            'aliases' => array(),
-            'reference' => '30897edbfb15e784fe55587b4f73ceefd3c4d98c',
-            'dev_requirement' => false,
-        ),
-        'phpmailer/phpmailer' => array(
-            'pretty_version' => 'v6.6.3',
-            'version' => '6.6.3.0',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
-            'aliases' => array(),
-            'reference' => '9400f305a898f194caff5521f64e5dfa926626f3',
-            'dev_requirement' => false,
-        ),
-        'ua-parser/uap-php' => array(
-            'pretty_version' => 'v3.9.14',
-            'version' => '3.9.14.0',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../ua-parser/uap-php',
-            'aliases' => array(),
-            'reference' => 'b796c5ea5df588e65aeb4e2c6cce3811dec4fed6',
-            'dev_requirement' => false,
-        ),
+    'reference' => 'ecd96b2313fe9534274fcc8a397986f2dea5c0e9',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ecd96b2313fe9534274fcc8a397986f2dea5c0e9',
     ),
+    'composer/ca-bundle' => 
+    array (
+      'pretty_version' => '1.3.3',
+      'version' => '1.3.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '30897edbfb15e784fe55587b4f73ceefd3c4d98c',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.6.3',
+      'version' => '6.6.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9400f305a898f194caff5521f64e5dfa926626f3',
+    ),
+    'twbs/bootstrap' => 
+    array (
+      'pretty_version' => 'v4.6.2',
+      'version' => '4.6.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e5643aaa89eb67327a5b4abe7db976f0ea276b70',
+    ),
+    'twitter/bootstrap' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v4.6.2',
+      ),
+    ),
+    'ua-parser/uap-php' => 
+    array (
+      'pretty_version' => 'v3.9.14',
+      'version' => '3.9.14.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b796c5ea5df588e65aeb4e2c6cce3811dec4fed6',
+    ),
+  ),
 );
