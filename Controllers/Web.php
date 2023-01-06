49940397071b2cca3ccd7db58f2b67ffdc37236e
leenh bustmante <hackingleenh@gmail.com>
2023-01-06 10:11:28 -0500

    delete web/demo

--- Controllers/Web.php
+++ Controllers/Web.php
@@ -143,11 +143,4 @@ class Web extends Controllers
         }
         die();
     }
-
-    public function demo()
-    {
-        dep(date('Y-m-d-H-i-s'));
-        dep(path_img_404());
-        dep(dir_recursos());
-    }
 }
